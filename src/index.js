78fc20ecab1ba538d07d97eae699bf572a80832e
111111.txt <1040325106@qq.com>
2017-11-02 17:03:02 +0800

    aaas

--- src/index.js
+++ src/index.js
@@ -4,14 +4,23 @@ import App from './router/index';
 import registerServiceWorker from './registerServiceWorker';
 import {createStore, combineReducers} from 'redux';
 import {Provider} from 'react-redux';
+<<<<<<< HEAD
 import SingleReducer from './reducers/SingleReducer';
 import DetailsReducer from './reducers/DetailsReducer';
 const recuders = combineReducers({
 	list: SingleReducer,
 	list1: SingleReducer,
 	ss:DetailsReducer,
+=======
+import FoodReducer from './reducers/FoodReducer';
+import NextReducer from './reducers/NextReducer';
+import SectionReducer from './reducers/SectionReducer';
+
+const recuders = combineReducers({
 	list: FoodReducer,
+	list1:NextReducer,
 	list2:SectionReducer
+>>>>>>> fe02de053b2e8f160ab7eb42d318045ee5eeb61d
 })
 
 const store = createStore(recuders, {});
